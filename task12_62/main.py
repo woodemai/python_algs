5ac5670f7df86830482317d02e25bfa118707b5f
woodemai <n.savchenkoo73@gmail.com>
2023-12-18 02:27:03 +0300

    add 62:

--- task12_62/main.py
+++ task12_62/main.py
@@ -1,67 +1,33 @@
-import math
-
-w = 5
-
-
-def insertion_sort(arr, left, right):
-    for i in range(left, right):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
 
-def swap(a, b):
-    temp = a
-    a = b
-    b = temp
+def select_opt(arr, k, low, high):
+    if low == high:
+        return arr[low]
 
+    pivot_index = partition(arr, low, high)
 
-def compare(a, b):
-    if a > b:
-        return 1
-    elif a == b:
-        return 0
+    length = pivot_index - low + 1
+
+    if length == k:
+        return arr[pivot_index]
+    elif k < length:
+        return select_opt(arr, low, pivot_index - 1, k)
     else:
-        return -1
+        return select_opt(arr, pivot_index + 1, high, k - length)
 
-def partition(list, left, right):
-
-
-
-
-def select_opt(list, k, left, right):
-    while True:
-        d = right - left + 1
-        if d <= w:
-            insertion_sort(list, left, right)
-            result = left + k - 1
-            break
-    dd = math.floor(d / w)
-    for i in range(1, dd):
-        insertion_sort(list, left + (i - 1) * w, left + i * w - 1)
-        (
-            list[left + i - 1],
-            list[left + (i - 1) * w + math.ceil(w / 2) - 1]) = (
-            list[left + (i - 1) * w + math.ceil(w / 2) - 1],
-            list[left + i - 1]
-        )
-
-    v = select_opt(list, math.ceil(dd / 2), left, left + dd - 1)
-
-    list[left], list[v] = list[v], list[left]
-    v = partition(list, left, right)
-    temp = v - left + 1
-    comparison_result = compare(k, temp)
-    if comparison_result == -1:
-        right = v - 1
-    elif comparison_result == 0:
-        result = v
-    else:  # comparison_result == +1
-        k -= temp
-        left = v + 1
 
 arr = [20, 12, 18, 16, 24, 10, 22, 14]
-print(select_opt(list, 1, 0, len(list) - 1))
+
+k1, k2 = 4, 5
+res1 = select_opt(arr.copy(), k1, 0, len(arr) - 1)
+res2 = select_opt(arr.copy(), k2, 0, len(arr) - 1)
+print("Медиана: ", (res1 + res2) / 2)
